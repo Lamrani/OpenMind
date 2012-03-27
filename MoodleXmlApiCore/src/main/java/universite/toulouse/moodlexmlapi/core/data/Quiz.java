bfada0cce51dd88dd6111b2f7b4493b19cebd92d
ticetime <github@ticetime.com>
2012-03-27 13:43:20 +0200

    Quiz import export service

--- MoodleXmlApiCore/src/main/java/universite/toulouse/moodlexmlapi/core/data/Quiz.java
+++ MoodleXmlApiCore/src/main/java/universite/toulouse/moodlexmlapi/core/data/Quiz.java
@@ -2,42 +2,11 @@ package universite.toulouse.moodlexmlapi.core.data;
 
 import java.util.List;
 
-/**
- * Interface representing a Moodle quiz
- * @author fsil 
- *
- */
 public interface Quiz {
 	
 	/**
-	 * @return the count of processed questions
+	 * @return the question list
 	 */
-	public int getProcessedQuestionCount();
-	
-	/**
-	 * @return the count of extracted questions
-	 */
-	public int getExtractedQuestionCount();
-	
-	/**
-	 * @return the count of non extracted questions
-	 */
-	public int getNonExtractedQuestionCount();
-	
-	/**
-	 * @return the list of processed questions
-	 */
-	public List<Question> getProcessedQuestionList();
-	
-	/**
-	 * @return the list of extracted questions
-	 */
-	public List<Question> getExtractedQuestionList();
-	
-	/**
-	 * @return the list of non extracted questions
-	 */
-	public List<Question> getNonExtractedQuestionList();
-	
+	public List<Question> getQuestionList();
 
 }
