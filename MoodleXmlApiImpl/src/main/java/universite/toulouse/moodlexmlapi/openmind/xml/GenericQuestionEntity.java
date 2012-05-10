8d8c3f5f9d3fb5d708207a11fdd0253eba5fb12b
Chabane <chabane.refes@gmail.com>
2012-05-10 08:50:01 +0200

    Refactoring

---
+++ MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/GenericQuestionEntity.java
@@ -0,0 +1,11 @@
+package universite.toulouse.moodlexmlapi.openmind.xml;
+
+import universite.toulouse.moodlexmlapi.core.data.GenericQuestion;
+
+public class GenericQuestionEntity extends GenericQuestion{
+
+	public Boolean isHidden() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/access/GenericQuestionAccess.java
+++ MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/access/GenericQuestionAccess.java
@@ -1,6 +1,34 @@
 package universite.toulouse.moodlexmlapi.openmind.xml.access;
 
+import org.jdom.Element;
+
+import universite.toulouse.moodlexmlapi.core.InvalidQuizFormatException;
+import universite.toulouse.moodlexmlapi.core.data.CategoryQuestion;
+import universite.toulouse.moodlexmlapi.core.data.GenericQuestion;
+import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+import universite.toulouse.moodlexmlapi.openmind.xml.GenericQuestionEntity;
+
 public class GenericQuestionAccess {
 
-	public void getQuestion(){}
+	/**
+	 * Get a category of question and course value
+	 * @return an instance of category question
+	 */
+	public static GenericQuestion getGenericQuestion(){
+		
+		GenericQuestion genericQuestion = new GenericQuestionEntity();
+		
+		try {
+			Element quizElement = DomAccess.getRootElement("quiz");
+			Element questionElement = DomAccess.getElement(quizElement, "question");
+			String typeElement =  DomAccess.getAttributtValue(questionElement, "type");
+			// Checking and add a value of type
+			if(QuestionType.values().equals(typeElement)){
+				genericQuestion.setQuestionType(QuestionType.valueOf(typeElement));
+			}
+		} catch (InvalidQuizFormatException e) {
+			e.printStackTrace();
+		}	
+		return genericQuestion;
+	}
 }
