5118136a4de82911d1fe1bf218e7a2732511a373
Chabane <chabane.refes@gmail.com>
2012-05-10 09:18:55 +0200

    Create a genericQuestion method

--- MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/access/GenericQuestionAccess.java
+++ MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/access/GenericQuestionAccess.java
@@ -5,7 +5,9 @@ import org.jdom.Element;
 import universite.toulouse.moodlexmlapi.core.InvalidQuizFormatException;
 import universite.toulouse.moodlexmlapi.core.data.CategoryQuestion;
 import universite.toulouse.moodlexmlapi.core.data.GenericQuestion;
+import universite.toulouse.moodlexmlapi.core.data.QuestionText;
 import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+import universite.toulouse.moodlexmlapi.core.data.QuestionTextFormat;
 import universite.toulouse.moodlexmlapi.openmind.xml.GenericQuestionEntity;
 
 public class GenericQuestionAccess {
@@ -21,11 +23,50 @@ public class GenericQuestionAccess {
 		try {
 			Element quizElement = DomAccess.getRootElement("quiz");
 			Element questionElement = DomAccess.getElement(quizElement, "question");
-			String typeElement =  DomAccess.getAttributtValue(questionElement, "type");
+			
+			/*
+			 * Type properties
+			 */
+			String type =  DomAccess.getAttributtValue(questionElement, "type");
 			// Checking and add a value of type
-			if(QuestionType.values().equals(typeElement)){
-				genericQuestion.setQuestionType(QuestionType.valueOf(typeElement));
+			if(QuestionType.values().equals(type)){
+				genericQuestion.setQuestionType(QuestionType.valueOf(type));
 			}
+			
+			/*
+			 * Name properties
+			 */
+			Element nameElement = DomAccess.getElement(questionElement, "name");
+			Element textElement = DomAccess.getElement(nameElement, "text");
+			String text =  DomAccess.getElementValue(textElement);
+			// Add a value of name
+			genericQuestion.setName(text);
+			
+			/*
+			 * QuestionText properties
+			 */
+			QuestionText questionText = null;
+			
+			Element questionTextElement = DomAccess.getElement(questionElement, "questiontext");
+			textElement = DomAccess.getElement(questionTextElement, "text");
+			text =  DomAccess.getElementValue(textElement);
+			
+			String format = DomAccess.getAttributtValue(questionTextElement, "format");
+			
+			// Checking and add a value of type
+			if(QuestionTextFormat.values().equals(format)){
+				questionText = new QuestionText(text, QuestionTextFormat.valueOf(format));
+			}
+			// Add an instance of questionText
+			genericQuestion.setQuestionText(questionText);
+			
+			/*
+			 * Image properties
+			 */
+			Element imageElement = DomAccess.getElement(questionElement, "image");
+			String image = DomAccess.getElementValue(imageElement);
+			// Add a value of image
+			genericQuestion.setImageUrl(image);
 		} catch (InvalidQuizFormatException e) {
 			e.printStackTrace();
 		}	
--- MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/manager/QuizManager.java
+++ MoodleXmlApiImpl/src/main/java/universite/toulouse/moodlexmlapi/openmind/xml/manager/QuizManager.java
@@ -3,10 +3,13 @@ package universite.toulouse.moodlexmlapi.openmind.xml.manager;
 import java.util.List;
 
 import org.jdom.Document;
+import org.jdom.Element;
 
+import universite.toulouse.moodlexmlapi.core.InvalidQuizFormatException;
 import universite.toulouse.moodlexmlapi.core.data.ImportedQuiz;
 import universite.toulouse.moodlexmlapi.core.data.Question;
 import universite.toulouse.moodlexmlapi.core.data.Quiz;
+import universite.toulouse.moodlexmlapi.openmind.xml.access.DomAccess;
 
 public class QuizManager implements ImportedQuiz, Quiz {
 
@@ -27,8 +30,17 @@ public class QuizManager implements ImportedQuiz, Quiz {
 	}
 
 	public int getProcessedQuestionCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Element quizElement = null;
+		List questions = null;
+		try {
+			quizElement = DomAccess.getRootElement("quiz");
+			questions = DomAccess.getListElement(quizElement, "question");
+		} catch (InvalidQuizFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return questions.size();
 	}
 
 	public int getExtractedQuestionCount() {
